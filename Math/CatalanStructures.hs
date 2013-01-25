1871b04275fabd7befa9cb315391cfefdecef2ec
Stuart Paton <stuart.john.paton@gmail.com>
2013-01-25 23:35:03 +0000

    updated typeclass Catalan and fixed comment on isDyckPath function

--- Math/CatalanStructures.hs
+++ Math/CatalanStructures.hs
@@ -11,11 +11,11 @@ import qualified Data.Text as T
 
 -- A Dyck path is an encoding where u is an up-step and d is a down-step
 type DyckPath = [Char]
-type Permutation = undefined
+type Permutation = String
 
 class Catalan where
-cons :: a -> a -> a
-decons :: a -> a -> a
+	cons :: a -> a -> a
+	decons :: a -> (a, a)
 
 {-----------------------------------------------------------------------------------
 	Tests that structures are structures.
@@ -25,10 +25,11 @@ decons :: a -> a -> a
 Tests if the given permutation is a 132- and 231-avoiding permutation.
 Known as a one-stack sortable permutation
 -----------------------}
-isStackSortPerm :: String -> Bool
+isStackSortPerm :: Permutation -> Bool
 isStackSortPerm sigma = sigma `S.avoids` ["132", "231"] 
 
 {----------------------
+Tests if the given DyckPath is actually a DyckPath and not just a [Char].
 Has to satisfy the conditions:
 Start with an up-step, never go below the x-axis, and must end on the x-axis.
 That is, must start with a "u", number of "u"'s must equal number of "d"'s and must end with a "d"
