edc8688ee8e63f10e7278c3e7746235e45a7f82e
patons02 <stuart.john.paton@gmail.com>
2013-02-04 17:44:44 +0000

    updated type class and added a function for bijections

--- Math/Internal.hs
+++ Math/Internal.hs
@@ -3,6 +3,12 @@ module Internal where
 type Permutation = [Integer]
 
 class Catalan a where
+	empty :: a
 	cons :: a -> a -> a
-	decons :: a -> (a, a)
+	decons :: a -> Maybe (a, a)
+
+--bijection :: (Catalan a, Catalan b) => a -> b
+--bijection w = case decons w of
+--		Nothing -> empty
+--		Just (u,v) -> cons (bijection u, bijection v)
 
