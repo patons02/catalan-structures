7267a563b3a3d5d74b730277f0bde7ab0166bb23
patons02 <stuart.john.paton@gmail.com>
2013-02-28 10:14:06 +0000

    updated StackSortPerm.hs

--- Math/StackSortPerm.hs
+++ Math/StackSortPerm.hs
@@ -14,10 +14,12 @@ import qualified Math.Sym.Plot as P
 
 import Internal
 
-type StackSortablePermutation = Permutation
+data StackSortablePermutation = Empty | Perm231 Permutation deriving (Eq, Ord, Show)
+
+--type StackSortablePermutation = Permutation
 
 instance Catalan StackSortablePermutation where
-	empty = []
+	empty = Empty
 	cons = mkIndec
 	decons = decompose
 
@@ -26,16 +28,19 @@ instance Catalan StackSortablePermutation where
 ------------------------------------------------------------}
 
 mkIndec :: StackSortablePermutation -> StackSortablePermutation -> StackSortablePermutation
-mkIndec alpha beta = alpha ++ [n] ++ beta
+mkIndec alpha beta = Perm231 (a ++ [n] ++ b)
 	where
-	n = length (alpha ++ beta) + 1
+	n = length (a ++ b) + 1
+	a = ssptoperm alpha
+	b = ssptoperm beta
 
 decompose :: StackSortablePermutation -> Maybe (StackSortablePermutation, StackSortablePermutation)
-decompose sigma = if S.avoids (permToString sigma) ["231"]
-		  then Just (removeHeadSnd $ break (l ==) sigma)
+decompose sigma = if (S.avoids (sspToString sigma) ["231"]) || (S.avoids (sspToString sigma) ["132"])
+		  then Just (pairTossp $ removeHeadSnd $ break (l ==) s)
 		  else Nothing
 	where
-	l = length sigma
+	l = length s
+	s = ssptoperm sigma
 
 removeHeadSnd :: (t, [a]) -> (t, [a])
 removeHeadSnd (alpha, beta) = (alpha, tail beta)
@@ -47,13 +52,29 @@ pi <+> rho =  pi ++ rho' rho
 	rho' (x:xs) = length pi + x : rho' xs 
 
 alpha :: Permutation -> StackSortablePermutation
-alpha pi = n : pi
+alpha pi = Perm231 (n : pi)
 	where
 	n = length pi + 1
+
+sspToString :: StackSortablePermutation -> String
+sspToString p = permToString $ ssptoperm p
+
+permTossp :: Permutation -> StackSortablePermutation
+permTossp p = case p of
+	[] -> Empty
+	a -> Perm231 a
+
+ssptoperm :: StackSortablePermutation -> Permutation
+ssptoperm p = case p of
+	Empty -> []
+	Perm231 a -> a
+
+pairTossp :: ([Int], [Int]) -> (StackSortablePermutation, StackSortablePermutation)
+pairTossp (a,b) = (Perm231 a, Perm231 b)
 
 {-----------------------------------------------------------
 	Graphics functions
 ------------------------------------------------------------}
 
 draw :: StackSortablePermutation -> IO ()
-draw perm = drawPerm perm
+draw perm = drawPerm $ ssptoperm perm
