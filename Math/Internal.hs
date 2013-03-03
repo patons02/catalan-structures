8f93156817e788baa2f0cf85655cee34ef07cb47
patons02 <stuart.john.paton@gmail.com>
2013-03-03 16:27:52 +0000

    added permutation statistics and added a check for increasing and decreasing sequences

--- Math/Internal.hs
+++ Math/Internal.hs
@@ -11,10 +11,11 @@ import Graphics.UI.Gtk
 import Graphics.Rendering.Diagrams.Core
 
 import qualified Math.Sym.Plot as P 
+import qualified Math.Sym.Internal as I
 
 type Permutation = [Int]
 
-type Perm0 = SV.Vector Int
+type PermVec = SV.Vector Int
 
 type DC = Diagram Cairo R2
 
@@ -28,11 +29,115 @@ bijection w = case decons w of
 		Nothing -> empty
 		Just (u,v) -> cons (bijection u) (bijection v)
 
-perm0toperm :: Perm0 -> Permutation
-perm0toperm = SV.toList
+permvectoperm :: PermVec -> Permutation
+permvectoperm = SV.toList
 
-permtoperm0 :: Permutation -> Perm0
-permtoperm0 = SV.fromList
+permtopermvec :: Permutation -> PermVec
+permtopermvec = SV.fromList
+
+{-
+isInc' :: Bool -> [Bool] -> Bool
+isInc' x xs = length (filter (== x) xs) == 0 
+-}
+
+isInc :: Permutation -> Bool
+isInc p = length (filter (== False) (isInc' p)) == 0
+	where
+	isInc' [] = [True]
+	isInc' (x:[]) = [True]
+	isInc' (x:y:xs) = [y > x] ++ isInc' xs
+
+isDec :: Permutation -> Bool
+isDec p = length (filter (== False) (isDec' p)) == 0
+	where
+	isDec' [] = [True]
+	isDec' (x:[]) = [True]
+	isDec' (x:y:xs) = [x > y] ++ isDec' xs 
+
+{-----------------------------------------------------------
+	Permutation Operations
+------------------------------------------------------------}
+compVec :: PermVec -> PermVec
+compVec p0 = permtopermvec $ complement $ permvectoperm p0
+	
+complement :: Permutation -> Permutation
+complement p = map (\x -> length p + 1 - x) p 
+
+revPerm :: Permutation -> Permutation
+revPerm p = reverse p
+
+{-----------------------------------------------------------
+	Permutation Statistics
+------------------------------------------------------------}
+
+asc :: Permutation -> Int
+asc =  I.asc . permtopermvec
+
+des :: Permutation -> Int
+des =  I.des . permtopermvec
+
+exc :: Permutation -> Int
+exc = I.exc . permtopermvec
+
+ldr :: Permutation -> Int
+ldr = I.ldr . permtopermvec
+
+rdr :: Permutation -> Int
+rdr = I.rdr . permtopermvec
+
+lir :: Permutation -> Int
+lir =   I.lir . permtopermvec
+
+rir :: Permutation -> Int
+rir =   I.rir . permtopermvec
+
+zeil :: Permutation -> Int
+zeil =   I.rdr . I.inverse . permtopermvec --zeil = rdr . i 
+
+comp :: Permutation -> Int
+comp =   I.comp . permtopermvec
+
+lmax :: Permutation -> Int
+lmax =   I.lmax . permtopermvec
+
+lmin :: Permutation -> Int
+lmin =   I.lmin . permtopermvec
+
+rmax :: Permutation -> Int
+rmax =   I.rmax . permtopermvec
+
+rmin :: Permutation -> Int
+rmin =   I.rmin . permtopermvec
+
+head :: Permutation -> Int
+head =   I.head . permtopermvec
+
+last :: Permutation -> Int
+last =   I.last . permtopermvec
+
+peak :: Permutation -> Int
+peak =   I.peak . permtopermvec
+
+valley :: Permutation -> Int
+valley =   I.vall . permtopermvec
+
+--lds :: Permutation -> Int
+--lds = permtopermvec I.lds
+
+--lis :: Permutation -> Int
+--lis = permtopermvec I.lis
+
+rank :: Permutation -> Int
+rank =   I.ep . permtopermvec
+
+cyc :: Permutation -> Int
+cyc =   I.cyc . permtopermvec
+
+--fix :: Permutation -> Int
+--fix = permtopermvec I.fix
+
+--slmax :: Permutation -> Int
+--slmax = permtopermvec I.slmax
 
 {-----------------------------------------------------------
 	Permutation Graphics
@@ -44,7 +149,7 @@ drawPerm perm = do
 	window <- windowNew
 	canvas <- drawingAreaNew
 	canvas `on` sizeRequest $ return (Requisition 256 256)
-	set window [windowTitle := "Permutation Matrix", containerBorderWidth := 10, containerChild := canvas ]
+	set window [windowTitle := "Permutation Int Matrix", containerBorderWidth := 10, containerChild := canvas ]
 	canvas `on` exposeEvent $ renderFigurePerm canvas perm
 	onDestroy window mainQuit
 	widgetShowAll window
