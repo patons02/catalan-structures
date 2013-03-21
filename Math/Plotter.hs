3f66e869cf66f51772b18ef78d15bd2bc0f80991
patons02 <stuart.john.paton@gmail.com>
2013-03-21 15:06:18 +0000

    tidyed up and removed some folders

--- Math/Plotter.hs
+++
@@ -1,8 +0,0 @@
--- Module      : Math.Plotter
--- Copyright   : (c) Stuart Paton 2013
--- License     : undecided
--- Maintainer  : Stuart Paton <stuart.john.paton@gmail.com>
-
-module Plotter where
-
-import Math.Sym.Plot
