cb7ba0f122c001a601482f98e877a03c36cddcf1
patons02 <stuart.john.paton@gmail.com>
2013-03-21 15:11:48 +0000

    removed test folder

--- test/Math/test.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
-module Test where
-
-import DyckPath
-import StackSortPerm
-import Internal
-
-exam1 :: DyckPath -> StackSortablePermutation
-exam1 = bijection ([U,U,U,D,D,D,U,U,D,U,D,D]::DyckPath)
--- test/cfiles/bijections.h
+++
@@ -1,4 +0,0 @@
-#ifndef BIJECTIONS_H_INCLUDED
-#define BIJECTIONS_H_INCLUDED
-long richards (const long *b, long len);
-#endif
